9f209586b8e5b2833635c7ff52a38a4a579a2200
abacate <rafael.schrott02@gmail.com>
2025-07-25 02:35:06 -0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,93 @@
+#include <Arduino.h>
+
+#define UnitPin 2
+#define DecimalPin 3
+#define SerialOPin 4
+#define OutputEnable 5
+#define LatchClock 6
+#define SerialClock 7
+#define LatchReset 8
+
+class C_5261AS{
+  const int digits [10];
+  public:
+    //false = DPin; true = UPin
+    bool active;
+    //para ativar os UPin e DPin é active LOW
+    int DPin;
+    int UPin;
+    //saida de dados em sequencia
+    int SerialPin;
+    int outputEnable;
+    int shiftClock;
+    int latchClock;
+    int reset;
+
+    C_5261AS(int d, int u, int ser, int oe, int shClk, int lcClk, int rst){
+      active = false;
+      DPin = d;
+      UPin = u,
+      SerialPin = ser;
+      outputEnable = oe;
+      shiftClock = shClk;
+      latchClock = lcClk;
+      reset = rst;  
+      printPins();
+      initPins();
+    }
+
+  private:
+    void printPins(){
+      Serial.print("active: ");
+      Serial.println(active ? "DPin": "UPin");
+      Serial.print("UnitPin: ");
+      Serial.println(UPin);
+      Serial.print("DecimalPin: ");
+      Serial.println(DPin);
+      Serial.print("Serial Output: ");
+      Serial.println(SerialPin);
+      Serial.print("OutputEnable: ");
+      Serial.println(outputEnable);
+      Serial.print("Shift Clock: ");
+      Serial.println(shiftClock);
+      Serial.print("Latch Clock: ");
+      Serial.println(latchClock);
+      Serial.print("Reset: ");
+      Serial.println(reset);
+    }
+
+    void initPins(){
+      pinMode(DPin, OUTPUT);
+      pinMode(UPin, OUTPUT);
+      pinMode(SerialPin, OUTPUT);
+      pinMode(outputEnable, OUTPUT);
+      pinMode(shiftClock, OUTPUT);
+      pinMode(latchClock, OUTPUT);
+      pinMode(reset, OUTPUT);
+
+      digitalWrite(DPin, HIGH);
+      digitalWrite(UPin, HIGH);
+      // Inicializa os pinos em estado LOW por segurança
+      digitalWrite(SerialPin, LOW);
+      digitalWrite(outputEnable, LOW);
+      digitalWrite(shiftClock, LOW);
+      digitalWrite(latchClock, LOW);
+      digitalWrite(reset, LOW);
+    }
+
+    void changeDisplay(){
+      digitalWrite(DPin, active ? HIGH : LOW);
+      digitalWrite(UPin, active ? LOW : HIGH);
+      active = false;
+    }
+
+};
+
+void setup(){
+  Serial.begin(9600);
+  C_5261AS shiftRe(DecimalPin,UnitPin,SerialOPin,OutputEnable,SerialClock,LatchClock, LatchReset);
+}
+
+void loop(){
+
+}
